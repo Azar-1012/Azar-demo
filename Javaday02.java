6effb0f91ca8624fb0cdc6f91794228a4f23eba3
Azar-1012 <azarleo1012@gmail.com>
2025-07-19 17:55:09 +0530

    Day02practice

--- Javaday02.java
+++ Javaday02.java
@@ -2,11 +2,13 @@
 import java.util.Scanner;
 
 class Javaday02{
+@SuppressWarnings("ConvertToTryWithResources")
 public static void main(String[] args){
     Scanner sc= new Scanner(System.in);
     System.out.println("Enter a String");
     String s1=sc.nextLine();
     sc.close();
+    @SuppressWarnings("StringBufferMayBeStringBuilder")
     StringBuffer sf=new StringBuffer(s1);
     System.out.println("Reversed String");
     System.out.println(sf.reverse());
