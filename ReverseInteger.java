a65780ca2eabf589c453a68837780198a733a752
Azar-1012 <azarleo1012@gmail.com>
2025-07-19 18:33:19 +0530

    ReverseInteger code

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,13 +1,18 @@
 public class ReverseInteger{
     public static void main(String[] args){
-
-        int num=12345;
-        int rev=0;
+        //method using algorithm
+        long num=12345l;
+        long rev=0;
         while(num!=0){
             rev=rev*10+num%10;
             num=num/10;
 
         }
         System.out.println("ReversedInteger: "+rev);
+
+
+        //method using stringbuffer
+        long num1=123456789l;
+        System.out.println(new StringBuffer(String.valueOf(num1)).reverse());
     }
 }
