55617454e2c160e2cadc073b1daeb8b4694b077e
Azar-1012 <azarleo1012@gmail.com>
2025-07-21 21:56:36 +0530

    Pattern Problem

--- Javaday03.java
+++ Javaday03.java
@@ -7,16 +7,17 @@ public class Javaday03 {
 
     System.out.println("Rightangled star pattern");
     System.out.println("Enter rows");
-    Scanner sc= new Scanner(System.in);
-    int n=sc.nextInt();
-     sc.close();
+    try (Scanner sc = new Scanner(System.in)) {
+        int n=sc.nextInt();
+         sc.close();
 
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<i;j++){
-            System.out.print("* ");
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("* ");
 
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 
     }
